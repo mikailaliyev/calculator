6e1d17d8ab5c5ae5b0df8867be23caf103dae44c
Mikayil Aliyev <mikail.aliyev@gmail.com>
2022-03-30 12:28:33 +0400

    minor design additions

--- media/scripts.js
+++ media/scripts.js
@@ -1,5 +1,18 @@
 const el = (htmlElement) => {
     if(htmlElement.charAt(0) === "#") {
         return document.querySelector(htmlElement)
+    } else {
+        return Array.from(document.querySelectorAll(htmlElement))
     }
 }
+
+
+//Changing colors of clicked buttons
+el(".ops").forEach(i => {
+   i.addEventListener("click", () => {
+       i.style.border = "3px solid green"
+    })
+})
+
+
+
