15c4c5c32e7a4520119df0ea26891eb3e3a9243e
Mikayil Aliyev <mikail.aliyev@gmail.com>
2022-03-31 17:00:22 +0400

    showing one math operator at a time

--- media/scripts.js
+++ media/scripts.js
@@ -1,33 +1,36 @@
 //Neat DOM elements picking
 const el = (htmlElement) => {
-    if(htmlElement.charAt(0) === "#") {
-        return document.querySelector(htmlElement)
-    } else {
-        return Array.from(document.querySelectorAll(htmlElement))
-    }
-}
-
-
+  if (htmlElement.charAt(0) === "#") {
+    return document.querySelector(htmlElement);
+  } else {
+    return Array.from(document.querySelectorAll(htmlElement));
+  }
+};
 
 //Changing colors of clicked buttons
-el(".ops").forEach(i => {
-   i.addEventListener("click", () => {
-       //Operations to screen
-        if(!['/','X','-','+','=','C','<','%'].includes(i.innerText)) {
-            el("#screen").innerText = i.innerText  
-        }
-        //Changing border color of math operators NOT WORKING YET!
-       if(['/','X','-','+','=','C','<','%'].includes(i.innerText) && i.style.border === null) {
-           i.style.border = "3px solid red"
-       }
+el(".ops").forEach((i) => {
+  i.addEventListener("click", () => {
+    //Operations to screen
+    if (!["/", "X", "-", "+", "=", "C", "<", "%"].includes(i.innerText)) {
+      el("#screen").innerText += i.innerText;
+    }
+    //Changing border color of math operators NOT WORKING YET!
+    if (["/", "X", "-", "+", "=", "C", "<", "%"].includes(i.innerText)) {
+      //Preventing operators to be shown more than once
+      ["/", "X", "-", "+", "=", "C", "<", "%"].includes(el("#screen").innerText.slice(-1))
+        ? el("#screen").innerText
+        : (el("#screen").innerText += i.innerText);
 
-       if(i.innerText === "=") {
-        el(".ops").forEach(i => {
-            i.style.border = null
-        })
-       }
-    })
-})
+      //   if (!i.style.border) i.style.border = "3px solid red";
+      //
+    }
 
-
-
+    //Equal does things
+    if (i.innerText === "=") {
+      el(".ops").forEach((i) => {
+        i.style.border = null;
+      });
+      el("#screen").innerText = "";
+    }
+  });
+});
